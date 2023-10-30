c29993239b59d9e69907498df3bb48e20a28a437
Quynh Ngan Dao <nguy2884@umn.edu>
2023-10-29 21:50:55 -0500

    added hero and search

---
+++ resources/views/partials/_hero.blade.php
@@ -0,0 +1,26 @@
+ {{-- HERO --}}
+ <section
+ class="relative h-72 bg-laravel flex flex-col justify-center align-center text-center space-y-4 mb-4"
+>
+ <div
+     class="absolute top-0 left-0 w-full h-full opacity-95 bg-no-repeat bg-center bg-right bg-white"
+     style="background-image: url('images/cat.jpeg')"
+ ></div>
+
+ <div class="z-10">
+     <h1 class="text-6xl font-bold uppercase text-indigo-950">
+         PHP<span class="text-black">et</span>
+     </h1>
+
+     <p class="text-2xl text-indigo-950 font-bold my-4">
+         Adopt or Play with Local Animals
+     </p>
+     <div>
+         <a
+             href="register.html"
+             class="inline-block border-2 border-indigo-900 text-indigo-900 py-2 px-4 rounded-xl uppercase mt-2 hover:text-black hover:border-black"
+             >Get Started</a
+         >
+     </div>
+ </div>
+</section>
