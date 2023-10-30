39a4a1a56561d6434457d8dafbfc299da622c924
Quynh Ngan Dao <nguy2884@umn.edu>
2023-10-29 19:10:31 -0500

    added layout and images

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,14 +1,44 @@
 @extends('layout')
 
 @section('content')
-<h1>{{$heading}}</h1>
-@unless (count($listings) == 0)
-    @foreach ($listings as $listing)
-        <h2><a href="/listings/{{ $listing['id'] }}">{{ $listing['name'] }}</a></h2>
-        <p>{{ $listing['location'] }}</p>
-        <p>{{ $listing['contact'] }}</p>
-    @endforeach
-@else
-    <p>No listings found</p>
-@endunless
+    <div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
+        @unless (count($listings) == 0)
+            @foreach ($listings as $listing)
+                <div class="bg-gray-50 border border-gray-200 rounded p-6">
+                    <div class="flex">
+                        {{-- <img class="hidden w-48 mr-6 md:block" src="images/petunia.jpeg" alt="" /> --}}
+                        <div>
+                            <h3 class="text-2xl">
+                                <a href="show.html">{{$listing->name}}</a>
+                            </h3>
+                            <div class="text-xl font-bold mb-4">{{$listing->organization}}</div>
+                            <ul class="flex">
+                                <li
+                                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs">
+                                    <a href="#">Dog</a>
+                                </li>
+                                <li
+                                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs">
+                                    <a href="#">Cat</a>
+                                </li>
+                                <li
+                                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs">
+                                    <a href="#">Rabbit</a>
+                                </li>
+                                <li
+                                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs">
+                                    <a href="#">Bird</a>
+                                </li>
+                            </ul>
+                            <div class="text-lg mt-4">
+                                <i class="fa-solid fa-location-dot"></i> {{$listing->location}}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        @else
+            <p>No listings found</p>
+        @endunless
+    </div>
 @endsection
